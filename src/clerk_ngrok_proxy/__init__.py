344bd9c8cad9b316940675b22040a182706e4464
Bryan Carson <carsonb@gmail.com>
2026-01-18 08:26:46 -0800

    First pass

---
+++ src/clerk_ngrok_proxy/__init__.py
@@ -0,0 +1,12 @@
+"""Clerk authentication proxy for custom domains (ngrok, tunnels, etc.)."""
+from clerkway.jwks import ClerkJWKClient, get_jwks_client
+from clerkway.proxy import ClerkProxyConfig, create_clerk_proxy_router
+
+__all__ = [
+    "ClerkJWKClient",
+    "ClerkProxyConfig",
+    "create_clerk_proxy_router",
+    "get_jwks_client",
+]
+
+__version__ = "0.1.0"
